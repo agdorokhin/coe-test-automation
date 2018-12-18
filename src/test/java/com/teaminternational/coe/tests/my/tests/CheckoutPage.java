4b18df258618a9b98210efa490e046ce3692f4c3
alexeyd <agdorokhin@gmail.com>
2018-12-18 17:17:07 +0200

    Add project to remote repo

--- src/test/java/com/teaminternational/coe/tests/my/tests/CheckoutPage.java
+++ src/test/java/com/teaminternational/coe/tests/my/tests/CheckoutPage.java
@@ -1,12 +1,26 @@
 package com.teaminternational.coe.tests.my.tests;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class CheckoutPage {
+    public SelenideElement checkoutWlcMsg = $(".subheader_label");
     SelenideElement firstNameField = $("[data-test='firstName']");
     SelenideElement lastNameField = $("[data-test='lastName']");
     SelenideElement zipCode = $("[data-test='postalCode']");
-    SelenideElement continueCheckoutBtn = $("cart_checkout_link");
+    SelenideElement continueCheckoutBtn = $(".cart_checkout_link");
+
+    public CheckoutPage() {
+        checkoutWlcMsg.shouldBe(Condition.visible);
+    }
+
+    public void ContinueCheckout (String firstName, String lastName, String postalCode) {
+        firstNameField.setValue(firstName);
+        lastNameField.setValue(lastName);
+        zipCode.setValue(postalCode);
+        continueCheckoutBtn.click();
+
+    }
 }
--- src/test/java/com/teaminternational/coe/tests/my/tests/LoginPage.java
+++ src/test/java/com/teaminternational/coe/tests/my/tests/LoginPage.java
@@ -15,6 +15,5 @@ public class LoginPage {
         password.setValue(pwd);
         loginBtn.click();
         //return new LoginPage();
-
     }
 }
